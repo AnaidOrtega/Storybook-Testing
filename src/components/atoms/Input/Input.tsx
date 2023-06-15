72abd7bdb04279a3aa8328871a69d68e6446e053
Itzel Ortega <itzel.ortega9819@hotmail.com>
2023-06-15 11:08:36 -0600

    Refactor Input Component

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC, useState } from "react";
+import React, { FC } from "react";
 import { Input as AntdInput, Typography, InputProps as AntdInputProps } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 
@@ -10,28 +10,18 @@ export interface InputProps extends AntdInputProps {
   error?: string;
 }
 
-export const Input: FC<InputProps> = ({ label, placeholder, id, error, onChange }) => {
-  const [value, setValue] = useState("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    if (onChange) onChange(e)
-  };
-
-  return (
-    <div>
-      {label && <Title level={3}>{label}</Title>}
-      <AntdInput
-        id={id}
-        size="large"
-        value={value}
-        data-testid={id}
-        onChange={handleChange}
-        prefix={<UserOutlined />}
-        placeholder={placeholder}
-        status={error ? "error" : ""}
-      />
-      {error && <Text type="danger">{error}</Text>}
-    </div>
-  );
-};
+export const Input: FC<InputProps> = ({ label, placeholder, id, error, onChange }) => (
+  <div>
+    {label && <Title level={3}>{label}</Title>}
+    <AntdInput
+      id={id}
+      size="large"
+      data-testid={id}
+      onChange={onChange}
+      prefix={<UserOutlined />}
+      placeholder={placeholder}
+      status={error ? "error" : ""}
+    />
+    {error && <Text type="danger">{error}</Text>}
+  </div>
+);
